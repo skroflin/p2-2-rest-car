ff988cea1255f7dad02e1a6c676c1c4afc192cc3
Sven Kroflin <99004305+skroflin@users.noreply.github.com>
2025-06-15 16:35:49 +0200

    Početak rada na VoziloController
    Početak rada na VoziloController. Dodana dva endpoint-a -get i getBySifra.

--- src/main/java/ffos/skroflin/controller/VoziloController.java
+++ src/main/java/ffos/skroflin/controller/VoziloController.java
@@ -4,10 +4,63 @@
  */
 package ffos.skroflin.controller;
 
+import ffos.skroflin.model.Vozilo;
+import ffos.skroflin.service.VoziloService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author svenk
  */
+@Tag(name = "Skroflin -> Vozilo", description = "Sve dostupne rute koje se odnose na entitet Vozilo. Rute se odnose na CRUD metode - create (post), read (get), update (put) i delete.")
+@RestController
+@RequestMapping("/api/skroflin/vozilo")
 public class VoziloController {
+    private final VoziloService voziloService;
+
+    public VoziloController(VoziloService voziloService) {
+        this.voziloService = voziloService;
+    }
     
+    @Operation(
+            summary = "Dohvaća sva vozila", tags = {"get", "vozilo"},
+            description = "Dohvaća sva vozila sa svim podacima"
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Vozilo.class)))),
+                @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+            })
+    @GetMapping("/get")
+    public ResponseEntity get(){
+        try {
+            return new ResponseEntity<>(voziloService.getAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GetMapping("/getBySifra")
+    public ResponseEntity getBySifra(
+            @RequestParam int sifra
+    ){
+        try {
+            if (sifra <= 0) {
+                return new ResponseEntity<>("Šifra mora biti veća od 0!" + " " + sifra, HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+        }
+    }
 }
